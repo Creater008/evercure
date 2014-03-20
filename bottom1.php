7dddb018b7037bb34d3fe13d9fcb8192fe9b3fda
Nishant <nishant_cs@hotmail.com>
2014-03-20 21:07:06 +0530

    Width
    A step towards responsiveness.

--- bottom1.php
+++ bottom1.php
@@ -1,11 +1,14 @@
-<div class="web-bottom">
+<div class="web-bottom" style="width:100%">
       <div class="left-bottom">
         <div class="left-bottom-wrap">
           <div style="display:inline-block;width:400px;padding:5px 2px;padding-bottom:1px;margin:0px 10px;color:#5f5d5d;border-bottom:1px solid #646467;display:inline-block;text-align:left;font:18px 'Times New Roman';">
             Disclaimer:
           </div>
           <p style="padding:0px;margin:0px;font:12px 'Times New Roman';text-align:justify;margin-left:10px;color:#7d7d82">
-            All logo used on this site other than EvercureHelp are trademarks of their respective owners. EvercureHelp is not connected with / or is a part of any of these other organizations. All the content on this website is owned and copyrighted by EvercureHelp. EvercureHelp is a trademark of Evercure IT Solution PVT. LTD. &reg; Services.</p>
+            All logo used on this site other than EvercureHelp are trademarks of their respective owners. 
+            EvercureHelp is not connected with / or is a part of any of these other organizations.
+             All the content on this website is owned and copyrighted by EvercureHelp.
+              EvercureHelp is a trademark of Evercure IT Solution PVT. LTD. &reg; Services.</p>
         </div>
       </div>
       <div class="right-bottom">
